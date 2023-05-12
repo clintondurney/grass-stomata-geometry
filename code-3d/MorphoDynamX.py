5b4da37575f86967ca7dab08310a720e51f152d6
Clinton H. Durney <clinton.durney@jic.ac.uk>
2023-05-12 08:02:02 -0700

    2d mesh for comparison purposes

---
+++ code-3d/MorphoDynamX.py
@@ -0,0 +1,9 @@
+######## New MorphoDynamX session v2.0 r2-1236: 2023-05-11 15:11:14
+
+Process.Stack__System__Reset_Stack('')
+Process.Tools__System__Save_View('/mnt/hgfs/durney/stomata/manuscript/grass-stomata-geometry/code-3d/FemWedges.mdxv')
+Process.Mesh__System__Open('', '', 'No', 'No')
+Process.Tools__System__Save_View('/mnt/hgfs/durney/stomata/manuscript/grass-stomata-geometry/code-3d/FemWedges.mdxv')
+Process.Tools__System__Load_View('FemWedges.mdxv')
+######## New MorphoDynamX session v2.0 r2-1236: 2023-05-11 15:12:06
+Process.Tools__System__Load_View('FemWedges.mdxv')
